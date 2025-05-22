9ce6f3b4d3953bf300680038c152d6b85496433a
Carl Bunn <72443857+carlbunn@users.noreply.github.com>
2025-05-22 13:14:55 +1000

    Update check-megasync.sh

--- check-megasync.sh
+++ check-megasync.sh
@@ -2,7 +2,7 @@
 # Check if mega-sync is running
 if ! pgrep -x "mega-sync" >/dev/null 2>&1; then
     echo "$(date): mega-sync not running, starting it..."
-    mega-sync &
+    mega-sync /mega_sync/ / &
 else
     echo "$(date): mega-sync is running"
 fi
